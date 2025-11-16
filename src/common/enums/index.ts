724ab3f47c3b90dcf188e190887b2fd83147fbd3
nicolasalmeida1 <ng.almeidaa@gmail.com>
2025-11-16 18:00:22 -0300

    feat(common): add payment enums (status, method, event, routes)

---
+++ src/common/enums/index.ts
@@ -0,0 +1,4 @@
+export * from './payment-status.enum';
+export * from './payment-method.enum';
+export * from './payment-event.enum';
+export * from './mercado-pago-routes.enum';
---
+++ src/common/enums/mercado-pago-routes.enum.ts
@@ -0,0 +1,5 @@
+export const MercadoPagoRoutes = {
+  CREATE_PREFERENCE: '/checkout/preferences',
+  GET_PREFERENCE: '/checkout/preferences',
+  GET_PAYMENT: '/v1/payments',
+} as const;
