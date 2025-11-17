1f83e1880111d6c7888d53d67f4e6e60bf286b86
nicolasalmeida1 <ng.almeidaa@gmail.com>
2025-11-17 07:06:43 -0300

    fix(server): alterar porta padrão para 3003

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
     }),
   );
 
-  const port = process.env.PORT || 3000;
+  const port = process.env.PORT || 3003;
   await app.listen(port, '0.0.0.0');
 
   console.log(`Server running on port ${port}`);
